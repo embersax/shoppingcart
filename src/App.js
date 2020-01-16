68732caf3f6976b6d3382dc7ce0b7b257a673451
embersax <ruilinzhou2022@u.northwestern.edu>
2020-01-15 22:44:59 -0600

    invetory implemented

--- src/App.js
+++ src/App.js
@@ -4,82 +4,78 @@ import {Card,Column,Image,Content,Level,Divider,Button,Navbar,Media,Title} from 
 import Sidebar from "react-sidebar";
 import CartCard from './Components/CartCard'
 import Cards from './Components/Cards'
-// const Cards=({product,state})=>{
-//   var setShowShoppingcart=Object.values(state)[1];
-//   var cartItems=Object.values(state)[2];
-//   var setCartItems=Object.values(state)[3];
-//   return(
-//       <Card key={product.sku}>
-//         <Card.Image>
-//           <Image.Container >
-//             <Image src={require('../public/data/products/'+product.sku+'_1.jpg')} />
-//           </Image.Container>
-//         </Card.Image>
-//         <Card.Content>
-//           <Content>
-//             {product.title}
-//             <Divider>
-//                 {product.price+'$'}
-//             </Divider>
-//               <Divider>
-//                   {product.description}
-//               </Divider>
+// const Cards=({state,product})=>{
+//     var setShowShoppingcart=state.setShowShoppingcart;
+//     var cartItems=state.cartItems;
+//     var setCartItems=state.setCartItems;
+//     // var stocks=Object.values(stock);
+//     // console.log(stock);
+//     var stock=state.stock;
+//     var setDataInstock=state.setDataInstock;
+//     // // console.log( dataInstock[product.sku]);
+//     // var item=dataInstock[product.sku];
+//     // let sizes=["S","M","L","XL"];
+//     // let newstock=dataInstock
+//     console.log( stock[product.sku]);
+//     // console.log( dataInstock[product.sku][size]);
+//     // console.log(product);
 //
 //
-//           </Content>
 //
-//         </Card.Content>
-//           <Button.Group>
-//               {['S','M','L','XL'].map(size=>
-//                   <Button onClick={() => {
-//                   setShowShoppingcart(true);
-//                   cartItems.push(product);
-//                   setCartItems(cartItems);
-//                   console.log(cartItems)
-//                   }
-//                   }>
+//     // sizes=sizes.filter((key) => {return item[key]>0})
 //
-//                       {size}</Button>)}
-//           </Button.Group>
-//       </Card>
-//   )
-// }
-
-// const cartCard = ({ item }) => {
-//     console.log("made a cartCard!!");
 //
-//     return (
-//         <Card>
+//     return(
+//         <Card key={product.sku}>
+//             <Card.Image>
+//                 <Image.Container >
+//                     <Image src={require('../public/data/products/'+product.sku+'_1.jpg')} />
+//                 </Image.Container>
+//             </Card.Image>
 //             <Card.Content>
-//                 <Media>
-//                     <Media.Item as="figure" align="left">
-//                         <Image.Container as="p" size={64}>
-//                             <Image
-//                                 // alt="64*64"
-//                                 src={require('../../public/data/products/'+item.sku+'_2.jpg')}
-//                             />
-//                         </Image.Container>
-//                     </Media.Item>
-//                     <Media.Item>
-//                         <Title as="p" size={4}>
-//                             {item.title}
-//                         </Title>
-//                         <Title as="p" subtitle size={6}>
-//                             {item.price}
-//                         </Title>
-//                     </Media.Item>
-//                 </Media>
+//                 <Content>
+//                     {product.title}
+//                     <Divider>
+//                         {product.price+'$'}
+//                     </Divider>
+//                     <Divider>
+//                         {product.description}
+//                     </Divider>
+//
+//
+//                 </Content>
+//
 //             </Card.Content>
+//             <Button.Group>
+//                 {["S","M","L","XL"].map(size=>
+//                     <Button onClick={() => {
+//                         console.log( stock);
+//                         setShowShoppingcart(true);
+//                         let index=cartItems.findIndex((item)=>{return item.product === product && item.size === size});
+//                         if (index !==-1){
+//                             cartItems[index].count++;
+//                         }else{
+//                             cartItems.push({product: product,size:size,count:1});
+//                         }
+//                         // console.log( dataInstock[product.sku][size]);
+//                         setCartItems(cartItems);
+//
+//                     }}>
+//
+//                         {size}</Button>)}
+//             </Button.Group>
+//
 //         </Card>
-//     );
-// };
-//test github
-
+//     )
+// }
 
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+  const [dataInstock,setDataInstock]=useState({});
+  // console.log(dataInstock['12064273040195392']);
+  //   const stock=Object.values(dataInstock);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -87,8 +83,25 @@ const App = () => {
       const json = await response.json();
       setData(json);
     };
+      const fetchStock = async () => {
+          const response1 = await fetch('./data/inventory.json');
+          const json1 = await response1.json();
+          setDataInstock(json1);
+      };
+    fetchStock();
     fetchProducts();
-  }, []);
+
+  },[]);
+
+  // useEffect(()=>{
+  //     const fetchStock = async () => {
+  //         const response = await fetch('./data/inventory.json');
+  //         const json = await response.json();
+  //         setDataInstock(json);
+  //     };
+  //     fetchStock();
+  // },[]);
+
     const [showShoppingCart,setShowShoppingcart]=useState(false);
     const [cartItems,setCartItems]=useState([]);
   return (
@@ -97,6 +110,7 @@ const App = () => {
             <Navbar.Brand>
                 Nu-Shopping Cart
             </Navbar.Brand>
+
               <Navbar.Menu>
                   <Navbar.Segment align="end">
                       <Button onClick={()=>{setShowShoppingcart(! showShoppingCart);
@@ -118,9 +132,9 @@ const App = () => {
           <Column.Group  >
               {[1, 2, 3, 4,].map(i => (
                   <Column key={i}>
-                      {products.slice(4*(i-1),4*i).map(product=>
+                      {products.slice(4*(i-1),4*i).map(product =>
                           <Level style={{display:"flex"}}>
-                              <Cards state={{showShoppingCart,setShowShoppingcart,cartItems,setCartItems}} product={product} />
+                              <Cards state={{showShoppingCart,setShowShoppingcart,cartItems,setCartItems,stock :dataInstock,setDataInstock :setDataInstock}} product={product} />
                           </Level>
                       )}
                   </Column>
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,12 +2,30 @@ import {Button, Card, Content, Divider, Image} from "rbx";
 import React from "react";
 
 
-const Cards=({product,state})=>{
-    var setShowShoppingcart=Object.values(state)[1];
-    var cartItems=Object.values(state)[2];
-    var setCartItems=Object.values(state)[3];
+const Cards=({state,product})=>{
+    var setShowShoppingcart=state.setShowShoppingcart;
+    var cartItems=state.cartItems;
+    var setCartItems=state.setCartItems;
+    // var stocks=Object.values(stock);
+    // console.log(stock);
+    var stock=state.stock;
+    var setDataInstock=state.setDataInstock;
+    // // console.log( dataInstock[product.sku]);
+    // var item=dataInstock[product.sku];
+    // let sizes=["S","M","L","XL"];
+    // let newstock=dataInstock
+    // var a=stock[product.sku]
+    // console.log( stock[product.sku]["S"]);
+    // console.log( dataInstock[product.sku][size]);
+    // console.log(product);
+
+
+
+    // sizes=sizes.filter((key) => {return item[key]>0})
+
+
     return(
-        <Card key={product.sku}>
+        <Card key={product.sku}  style={{height:"100%", display:"flex", flexDirection:"column"}}>
             <Card.Image>
                 <Image.Container >
                     <Image src={require('../../public/data/products/'+product.sku+'_1.jpg')} />
@@ -28,8 +46,9 @@ const Cards=({product,state})=>{
 
             </Card.Content>
             <Button.Group>
-                {['S','M','L','XL'].map(size=>
+                {["S","M","L","XL"].filter((key) => {return stock[product.sku][key]>0}).map(size=>
                     <Button onClick={() => {
+                        // console.log( stock);
                         setShowShoppingcart(true);
                         let index=cartItems.findIndex((item)=>{return item.product === product && item.size === size});
                         if (index !==-1){
@@ -37,7 +56,7 @@ const Cards=({product,state})=>{
                         }else{
                             cartItems.push({product: product,size:size,count:1});
                         }
-
+                        console.log( stock[product.sku][size]);
                         setCartItems(cartItems);
 
                     }}>
